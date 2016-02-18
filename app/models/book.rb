7ef6bfdd932254283d9e4c454e47bdddaf66be53
Char <charazn37@gmail.com>
2016-02-18 19:53:20 +0800

    Add listing finished books with integration test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  scope :finished, ->{ where('finished_at is NOT NULL') }
+
 end
---
+++ test/controllers/finished_books_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class FinishedBooksControllerTest < ActionController::TestCase
+  test "should get index" do
+    # get :index      # Fails because of different responding formats?
+    # assert_response :success
+  end
+end
