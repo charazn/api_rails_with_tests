ee5bd1989ff7ca058be40d7b1ac5b891eb7d8f1c
Char <charazn37@gmail.com>
2016-02-19 14:18:34 +0800

    Add Genre model with integration_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
   resources :books, only: [:index, :create, :destroy]
   resources :finished_books, only: :index
+  resources :genres, only: :index
 end
