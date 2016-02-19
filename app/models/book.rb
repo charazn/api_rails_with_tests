ed7feebac7e2756f57aa0c48f14db639c23b5da3
Char <charazn37@gmail.com>
2016-02-19 14:46:02 +0800

    Add relationship between genre and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
+  belongs_to :genre
+
   validates :title, presence: true
 
   scope :finished, ->{ where('finished_at is NOT NULL') }
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -2,20 +2,25 @@ require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
-    Book.create!(title: "Pragmatic Programmer", rating: 5)
-    Book.create!(title: "Hunger Game", rating: 1)
+    @fiction = Genre.create!(name: "Fiction")
+    @fiction.books.create!(title: "Star Trek", rating: 5)
+    @fiction.books.create!(title: "Hunger Game", rating: 1)
   end
 
   test "listing books" do
-    get '/books'
+    get "/books"
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal Book.count, json(response.body)[:books].size
+
+    books = json(response.body)[:books] # Returns an array of books (each a Hash of book information)
+    assert_equal Book.count, books.size
+    book = Book.find(books.first[:id])
+    assert_equal @fiction.id, book.genre.id
   end
 
   test "list top rated books" do
-    get '/books?rating=5'
+    get "/books?rating=5"
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
